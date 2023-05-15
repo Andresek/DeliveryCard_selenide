3ecac1370af5a8d106bebb312a28a9221dd9b695
Andrew S <samum_94@mail.ru>
2023-05-15 18:44:39 +0300

    Coreccted test and added method of data generate

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -15,14 +16,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryCardTest {
 
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    String planningDate = generateDate(3, "dd.MM.yyyy");
+
 
     @Test
-    void formSubmission()  {
-
-        LocalDate date = LocalDate.now(); // получаем текущую дату
-        date = date.plusDays(3); // добавляем 3 дня к текущей дате
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy"); // необходимый формат даты для заяки
-        String data = dateFormatter.format(date);
+    void formSubmissionManualInput() {
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
@@ -30,7 +32,7 @@ class DeliveryCardTest {
         city.$("[class='input__control']").setValue("Йошкар-Ола");
         SelenideElement dateApp = $("[data-test-id='date']");
         dateApp.$("[class='input__control']").sendKeys(Keys.CONTROL + "a", Keys.DELETE); // очистка поля с датой
-        dateApp.$("[class='input__control']").setValue(data);
+        dateApp.$("[class='input__control']").setValue(planningDate);
         SelenideElement name = $("[data-test-id='name']");
         name.$("[class='input__control']").setValue("Сидоров Иван");
         SelenideElement phone = $("[data-test-id='phone']");
@@ -40,6 +42,29 @@ class DeliveryCardTest {
         $$("button").find(Condition.exactText("Забронировать")).click();
         Configuration.timeout = 15_000;
         SelenideElement notification = $("[data-test-id='notification']").shouldBe(visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
+
+  /*  @Test
+    void formSubmissionEnteringAutoCompletion() {
+        LocalDate date = LocalDate.now(); // получаем текущую дату
+        date = date.plusDays(7); // добавляем 3 дня к текущей дате
+        int dayOfMonth = date.getDayOfMonth();
+
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        SelenideElement city = $("[data-test-id='city']");
+        city.$("[class='input__control']").setValue("си");
+        $$("[class = 'menu-item__control']").last().click();
+        SelenideElement dateApp = $("[data-test-id='date']");
+        dateApp.$("[class = 'icon-button__content']").click();
+        SelenideElement days = $("[class = 'calendar__day']");
+        days.$("[attr = 'role']", dayOfMonth).click();
+    }*/
+
+
 }
 
